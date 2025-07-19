c6b3636a1199b4da16fb243571fe0553f84a25d0
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-07-18 22:20:03 -0300

    chore: applying lint in files generated by Angular

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -4,7 +4,7 @@ import { App } from './app';
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App]
     }).compileComponents();
   });
 
@@ -18,6 +18,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pseudo-pizza');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, pseudo-pizza'
+    );
   });
 });
