fff473bec12b67b9cdd5b9a4eda24c3b14287e69
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-08-02 17:51:30 -0300

    feat: add footer component

--- src/app/core/layout/ui/footer/footer.ts
+++ src/app/core/layout/ui/footer/footer.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
+import { LucideAngularModule, Mail, MapPin, Phone } from 'lucide-angular';
+
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
-export class Footer {}
+export class Footer {
+  protected readonly lucideIcons = {
+    mapPin: MapPin,
+    phone: Phone,
+    mail: Mail
+  };
+
+  protected readonly currentYear = new Date().getFullYear();
+}
